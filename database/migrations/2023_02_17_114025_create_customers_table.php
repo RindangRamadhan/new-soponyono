60a862fa2156556a2cd888338b4066f1672bdb3d
abyjebrut8 <abyjebrut@gmail.com>
2023-02-18 19:45:01 +0700

    migration customer

---
+++ database/migrations/2023_02_17_114025_create_customers_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('uid_id')->nullable();
+            $table->unsignedBigInteger('up3_id')->nullable();
+            $table->unsignedBigInteger('ulp_id')->nullable();
+            $table->unsignedBigInteger('id_pel')->nullable();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('tarif');
+            $table->unsignedInteger('daya')->nullable();
+            $table->string('kogol');
+            $table->string('gardu');
+            $table->text('address')->nullable();
+            $table->enum('status', ['Menunggak', 'Lunas']);
+            $table->auditable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('uid_id')
+                ->references('id')
+                ->on('uids');
+
+            $table->foreign('ulp_id')
+                ->references('id')
+                ->on('ulps');
+
+            $table->foreign('up3_id')
+                ->references('id')
+                ->on('up3s');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
