60d8ef78eba75671802f6372045d895b66fe5451
abyjebrut8 <abyjebrut@gmail.com>
2023-02-19 18:01:53 +0700

    label di button delete

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -372,7 +372,7 @@ class Helper
             $id = $record->id;
             $name = $record->name;
             if (!$name) {
-                $name = $record->user_name;
+                $name = '';
             }
             $buttonDownload = "<a href=" . url("$url/$id/download") . " class='btn btn-icon rounded-circle btn-primary btn-download-$id' data-id='$id'><i class='bx bx-download'></i></a>";
 
@@ -389,7 +389,7 @@ class Helper
                 : "";
 
             $buttonDelete = Auth::user()->can("$menu-$menu Hapus")
-                ? "<button type='button' class='btn btn-icon rounded-circle btn-danger btn-delete' data-id=" . $id . " data-name=" .$name. "><i class='bx bx-trash'></i></button>"
+                ? "<button type='button' class='btn btn-icon rounded-circle btn-danger btn-delete'  data-id=" . $id . " data-name='$name' ><i class='bx bx-trash'></i></button>"
                 : "";
 
             $record->action = "";
