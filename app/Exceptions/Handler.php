9d48705e5698c4f71de22d1b1f0c00f9c56c9817
Rindang Ramadhan <rindangramadhan10@gmail.com>
2023-03-07 00:27:09 +0700

    chore(handler) : - custome response api when unauthorized

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,8 +43,15 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (\Illuminate\Auth\AuthenticationException$e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json(
+                    [
+                        'success' => false,
+                        'message' => 'Unauthorized',
+                    ], 401
+                );
+            }
         });
     }
 }
