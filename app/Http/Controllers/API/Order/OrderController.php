040f0763e0326a7d0f38fff9228c9c83e939bdf4
Rindang Ramadhan <rindangramadhan10@gmail.com>
2023-03-07 00:29:33 +0700

    feat(order) : - create api get list order - create api store order

---
+++ app/Http/Controllers/API/Order/OrderController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\API\Order;
+
+use App\Helpers\Helper;
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Intervention\Image\ImageManagerStatic as Image;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+
+        DB::beginTransaction();
+
+        try {
+            $data = Order::select(
+                'orders.id', 'orders.customer_id', 'customer.name as customer_name', 'customer.address as customer_address',
+                'orders.tarif', 'orders.power', 'orders.class', 'orders.substation', 'orders.bill', 'orders.billing_status', 'orders.status'
+            )
+                ->join('customers AS customer', 'orders.customer_id', 'customer.id')
+                ->where('orders.user_id', $user->id)
+                ->where('orders.status', 'Open')
+                ->whereMonth('orders.created_at', date('m'))
+                ->get();
+
+            Order::where('user_id', $user->id)
+                ->where('status', 'Open')
+                ->whereMonth('created_at', date('m'))
+                ->update([
+                    'status' => 'On Progress',
+                ]);
+
+            DB::commit();
+        } catch (\Throwable$th) {
+            DB::rollBack();
+            throw $th;
+        }
+
+        return Helper::ResponseWriter('Successfully get orders', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'order_id' => 'required',
+            'billing_status' => 'required|in:Paid,Debt,Unpaid',
+            'due_date' => 'required_if:billing_status,Debt',
+        ]);
+
+        try {
+            $photos = "";
+
+            if ($request->hasFile('photo_1')) {
+                $photo_1 = $request->file('photo_1');
+
+                if (is_array($photo_1) || is_object($photo_1)) {
+                    $extension = $photo_1->getClientOriginalExtension();
+                    $filename = Str::random(40) . '.' . $extension;
+
+                    $img = Image::make($photo_1->getRealPath());
+                    $img->save(public_path('images/upload/' . $filename));
+                    $img->resize(255, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                    });
+
+                    $photos .= $filename;
+                }
+            }
+
+            if ($request->hasFile('photo_2')) {
+                $photo_2 = $request->file('photo_2');
+
+                if (is_array($photo_2) || is_object($photo_2)) {
+                    $extension = $photo_2->getClientOriginalExtension();
+                    $filename = Str::random(40) . '.' . $extension;
+
+                    $img = Image::make($photo_2->getRealPath());
+                    $img->save(public_path('images/upload/' . $filename));
+                    $img->resize(255, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                    });
+
+                    $photos .= ",$filename";
+                }
+            }
+
+            Order::find($request->order_id)
+                ->update([
+                    'status' => 'Done',
+                    'photos' => $photos,
+                    'due_date' => $request->due_date,
+                    'phone_number' => $request->phone_number,
+                    'billing_status' => $request->billing_status,
+                    'updated_by' => Auth::user()->id,
+                ]);
+
+            $data = [
+                'order_id' => $request->order_id,
+            ];
+
+            return Helper::ResponseWriter("Successfully upload order", $data, 201);
+        } catch (\Throwable$th) {
+            throw $th;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\Order\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +13,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Auth
+Route::prefix('/auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('/orders', OrderController::class);
 });
