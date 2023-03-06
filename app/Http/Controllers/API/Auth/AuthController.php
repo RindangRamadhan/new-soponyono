14ff613627be77b746ad4f25c177768c5b675ae8
Rindang Ramadhan <rindangramadhan10@gmail.com>
2023-03-07 00:29:07 +0700

    feat(auth) : - create api login - create api logout

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Helpers\Helper;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'user_name' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only(['user_name', 'password']))) {
+            return Helper::ResponseWriter('Username or Password does not match with our record', null, 401);
+        }
+
+        $user = User::where('user_name', $request->user_name)->first();
+
+        return Helper::ResponseWriter(
+            'Log in successfull',
+            [
+                '_token' => $user->createToken("API TOKEN")->plainTextToken,
+            ],
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return Helper::ResponseWriter('Logged out successfull');
+    }
+}
